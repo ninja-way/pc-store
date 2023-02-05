1acd722ac1f40198f8f18901ab3525b66c1d29ec
Pasha Baran <pashabaran1207@gmail.com>
2023-02-05 22:46:14 +0200

    Add gin instead net/http for routing and logging

--- internal/transport/server.go
+++ internal/transport/server.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"log"
 	"net/http"
-	"os"
 	"os/signal"
 	"syscall"
 	"time"
@@ -12,65 +11,43 @@ import (
 
 // Server stores the config and connected database
 type Server struct {
-	s      *http.Server
-	h      *Handler
-	errors chan error
+	s *http.Server
 }
 
 // NewServer is server constructor
-func NewServer(addr string, mw http.Handler, h *Handler) *Server {
+func NewServer(addr string, handler http.Handler) *Server {
 	return &Server{
 		s: &http.Server{
 			Addr:    addr,
-			Handler: mw,
+			Handler: handler,
 		},
-		h:      h,
-		errors: make(chan error),
 	}
-}
-
-func (s *Server) setupHandlers() {
-	http.HandleFunc("/computers", s.h.getComputers)
-	http.HandleFunc("/computer", s.h.addComputer)
-	http.HandleFunc("/computer/", s.h.manageComputer)
 }
 
 // Run configures and starts the server
-func (s *Server) Run() error {
-	s.setupHandlers()
+func (s *Server) Run() {
+	// track interrupts
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
 
+	// run server
 	go func() {
-		if err := s.s.ListenAndServe(); err != nil {
-			s.errors <- err
+		if err := s.s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Listen: %s\n", err)
 		}
 	}()
-
-	// chanel for track interrupts
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
-
 	log.Println("Server running...")
 
-	// stop server in both cases
-	select {
-	case <-quit:
-		s.stop()
-	case err := <-s.errors:
-		s.stop()
-		return err
-	}
-	return nil
-}
+	<-ctx.Done()
+	cancel()
 
-// Shuts down the server and close db connection
-func (s *Server) stop() {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	// gracefully shutdown the server
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	if err := s.s.Shutdown(ctx); err != nil {
 		log.Printf("Server Shutdown: %v", err)
 	}
 
-	//close(s.errors)
 	log.Println("Server stopped")
 }
