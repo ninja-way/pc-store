b502edb7851547fd69a6b6e2467441caf32ed93a
Pasha Baran <pashabaran1207@gmail.com>
2023-02-06 12:40:07 +0200

    Prapare config system(does not work on Windows)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"github.com/ninja-way/pc-store/internal/config"
 	"github.com/ninja-way/pc-store/internal/repository/postgres"
 	"github.com/ninja-way/pc-store/internal/service"
 	"github.com/ninja-way/pc-store/internal/transport"
 	"log"
 )
 
+const (
+	CONFIG_DIR  = "configs"
+	CONFIG_FILE = "main"
+)
+
 func main() {
+	cfg, err := config.New(CONFIG_DIR, CONFIG_FILE)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(cfg)
+
 	ctx := context.Background()
 
 	// connect to database
